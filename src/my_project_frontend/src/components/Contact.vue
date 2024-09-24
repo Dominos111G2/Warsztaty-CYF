4ea4c8b94f5b495b022ebb04f1de8cb2a57b83a9
Dominik Szczerbal <milosz.bajer@wp.pl>
2024-09-24 17:50:29 +0000

    Feat: Frontend

---
+++ src/my_project_frontend/src/components/Contact.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="contact-list">
+      <a href="https://github.com/Dominos111G2" target="_blank" class="contact-item">
+        GitHub
+      </a>
+      <a href="mailto:domin.szczerbal@wp.pl" class="contact-item">
+        Email
+      </a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  text-align: center;
+  margin-bottom: 16px;
+  padding: 20px;
+  background: linear-gradient(to right, #3c0b0b, #8f1e1e);
+  color: #ffffff;
+  border-radius: 8px;
+  max-width: 1200px;
+  width: 100%;
+  max-height: 400px;
+  height: 100%;
+}
+h1 {
+  font-size: 4rem;
+  margin-bottom: 10px;
+}
+h2 {
+  font-size: 2rem;
+  color: #76c7c0;
+}
+.contact-list {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+.contact-item {
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+.contact-item i {
+  margin-right: 8px;
+  font-size: 1.5rem;
+}
+.contact-item:hover {
+  color: #bb1b1b;
+}
+</style>
---
+++ src/my_project_frontend/src/components/aboutYou.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="about">
+    <h2>About You</h2>
+    <div class="about-content">
+      <p>{{ aboutText }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+  data() {
+    return {
+      aboutText: 'I am a high school student at a technical school, passionate about programming and developing my skills in this field. In my free time, I enjoy listening to music and playing the drums, which is one of my greatest passions.'
+    };
+  }
+}
+</script>
+
+<style scoped>
+.about {
+  padding: 35px 20px;
+  background: #1c1c1e;
+  color: #ffffff;
+  max-width: 1200px;
+  width: 100%;
+  border-radius: 8px;
+}
+.about h2 {
+  margin-left: 10px;
+}
+.about-content {
+  margin: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid #ec5252;
+  border-radius: 8px;
+}
+</style>
